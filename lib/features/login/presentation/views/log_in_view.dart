4cc039cc5f88969109a16c36db342dc1ea4dee42
youssef hamada <youssef.hamada0101@gmail.com>
2026-03-01 23:57:46 +0200

    create login view

---
+++ lib/features/login/presentation/views/log_in_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LogInView extends StatelessWidget {
+  const LogInView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Log In')),
+      body: Center(child: Text('Log In View')),
+    );
+  }
+}
