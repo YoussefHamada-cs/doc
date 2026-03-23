6bb065cbac4eeff016e772b379f646715bcc8d54
youssef hamada <youssef.hamada0101@gmail.com>
2026-03-23 19:28:03 +0200

    feat: add register route and view to AppRouter

--- lib/core/routing/app_routes.dart
+++ lib/core/routing/app_routes.dart
@@ -2,5 +2,6 @@ class AppRoutes {
   static const String splash = '/splash';
   static const String onboarding = '/onboarding';
   static const String logIn = '/logIn';
+  static const String register = '/register';
   static const String home = '/home';
 }
