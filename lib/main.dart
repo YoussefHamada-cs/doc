9441422b0f9ec20e8a0762676eff5acd42e7bc9e
youssef hamada <youssef.hamada0101@gmail.com>
2026-03-26 12:36:13 +0200

    Add Firebase Crashlytics integration and update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:duc/core/di/get_it_sevice.dart';
 import 'package:duc/doc_app.dart';
 import 'package:flutter/material.dart';
@@ -6,15 +9,30 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
- await ScreenUtil.ensureScreenSize();
-
-  if (appFlavor == 'production') {
-    
-  } else if (appFlavor == 'developer') {
-    
-  }
+  
+ 
+  await Firebase.initializeApp();
 
  
+  if (appFlavor == 'production') {
+    
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+  } else if (appFlavor == 'developer') {
+   await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+  }
+
+  // 3. التقاط أخطاء Flutter Framework (مثل أخطاء الـ UI)
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+
+  // 4. التقاط الأخطاء التي تحدث خارج Flutter (مثل أخطاء الـ Async)
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+  await ScreenUtil.ensureScreenSize();
   setupGetIt();
   
   runApp(const DocApp());
