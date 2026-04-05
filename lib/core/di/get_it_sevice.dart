c0d190d63e9470ab62d066f44c9774a2979cc64c
youssef hamada <youssef.hamada0101@gmail.com>
2026-04-05 02:12:40 +0200

    feat: implement home screen features including secure storage, cubit state management, and specialized doctor listing sections

--- lib/core/di/get_it_sevice.dart
+++ lib/core/di/get_it_sevice.dart
@@ -1,9 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:duc/core/api/api_consumer.dart';
 import 'package:duc/core/api/dio_consumer.dart';
-import 'package:duc/features/auth/data/repo/auth_repo_impl.dart';
+import 'package:duc/features/auth/data/repo_impl/auth_repo_impl.dart';
 import 'package:duc/features/auth/domain/controllers/user_cubit/auth_cubit.dart';
 import 'package:duc/features/auth/domain/repo/aurh_repo.dart';
+import 'package:duc/features/home/data/home_repo_impl/home_repo_impl.dart';
+import 'package:duc/features/home/domain/controllers/home_cubit/home_cubit.dart';
+import 'package:duc/features/home/domain/home_repo/home_repo.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
@@ -14,8 +17,16 @@ void setupGetIt() {
   getIt.registerLazySingleton<ApiConsumer>(
     () => DioConsumer(dio: getIt<Dio>()),
   );
+
+  // auth
   getIt.registerLazySingleton<AuthRepo>(
     () => UserRepoImpl(apiConsumer: getIt<ApiConsumer>()),
   );
   getIt.registerFactory(() => AuthCubit(userRepo: getIt<AuthRepo>()));
+
+  // home
+  getIt.registerLazySingleton<HomeRepo>(
+    () => HomeRepoImpl(getIt<ApiConsumer>()),
+  );
+  getIt.registerFactory(() => HomeCubit(getIt<HomeRepo>()));
 }
